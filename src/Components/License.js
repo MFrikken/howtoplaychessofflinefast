f3f72435c70a512a28096ba9afcd8917a1b4c6de
mfrinken <milan.frinken@hs-osnabrueck.de>
2023-06-13 13:54:56 +0200

    Changed license-page according to GNU GPL v3

--- src/Components/License.js
+++ src/Components/License.js
@@ -4,15 +4,34 @@ import React from 'react';
 function License() {
 
     return (
-        <div>
-            Copyright (C) 2023 howtoplaychessofflinefast.dev, Frinken Milan
+            <div className="license-text">
+                <h2>Copyright (c) 2023 Frinken Milan</h2>
+                <br/>
+                <a className="link" href={"https://www.gnu.org/licenses/gpl-3.0-standalone.html"}>COPYING</a>
+                <br/>
+                <a className="link" href={"https://gitlab.hs-osnabrueck.de/mfrinken/howtoplaychessofflinefast.git"}>GitLab</a>
+                <br/>
+                <br/>
+                How To Play Chess Offline (fast) is free software: you can redistribute it and/or modify
+                it under the terms of the GNU General Public License as published by
+                the Free Software Foundation,
+                <br/>
+                either version 3 of the License, or
+                (at your option) any later version.
 
-            How To Play Chess Offline (fast) is made available under the terms of the GNU General Public License 3 which is available at
+                This program is distributed in the hope that it will be useful,
+                but WITHOUT ANY WARRANTY; without even the implied warranty of
+                MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+                GNU General Public License for more details.
 
-            <a href={"https://www.gnu.org/licenses/gpl-3.0.html"}> https://www.gnu.org/licenses/gpl-3.0.html </a>
+                You should have received a copy of the GNU General Public License
+                along with this program.  If not, see <a href={"https://www.gnu.org/licenses/"}>www.gnu.org/licenses/</a>.
+                <br/>
+                <br/>
+                <br/>
 
-            see <a href={"https://www.gnu.org/licenses/gpl-3.0-standalone.html"}> LICENSE</a>
-        </div>
+            </div>
+
     );
 }
 
