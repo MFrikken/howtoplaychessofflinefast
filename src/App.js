e8d7d21d9dafb6438300399693ee1cfc206d3e7e
mfrinken <milan.frinken@hs-osnabrueck.de>
2023-06-28 18:34:28 +0200

    Changed to old BrowserRouter in index.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import License from "./Components/License";
 import Main from "./Components/Main";
 
 function App() {
 
     return (
-        <BrowserRouter>
             <Routes>
                 <Route exact path={"/"} element={<Main />} />
                 <Route exact path={"/License"} element={<License/>} />
             </Routes>
-        </BrowserRouter>
     );
 }
 
